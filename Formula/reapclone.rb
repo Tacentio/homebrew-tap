a6dbbfd90f0b26e56d1366480432d54630fb64c0
Daniel Moore <danielmoore.info@gmail.com>
2023-05-03 10:20:10 +1000

    reapclone 1.0.2

--- Formula/reapclone.rb
+++ Formula/reapclone.rb
@@ -4,9 +4,9 @@
 class Reapclone < Formula
   desc "CLI tool to download GitHub organisation/users repositories"
   homepage "https://github.com/tacentio/reapclone"
-  url "https://github.com/Tacentio/reapclone/releases/download/v1.0.1/reapclone_v1.0.1_x86_64-apple-darwin.zip"
-  sha256 "8ca529718c25f9585889436d7c8036fe1d53fe050c023194a507d597cc5b7260"
-  version "1.0.1"
+  url "https://github.com/Tacentio/reapclone/releases/download/v1.0.2/reapclone_v1.0.2_x86_64-apple-darwin.zip"
+  sha256 "3257c6a833213083619ba03ffb960f005b47845a9cdedb8f02660179a2dea13f"
+  version "1.0.2"
 
   def install
     bin.install "reapclone"
